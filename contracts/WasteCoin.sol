635ba66288be9bdf07906a48e028432905649426
Trevor Lazarus <mike@Trevors-MacBook-Pro.local>
2017-10-25 16:08:32 +0530

    adding waste-coin crowdsale contract

---
+++ contracts/WasteCoin.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.13;
+
+import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+
+contract WasteCoin is MintableToken {
+
+string public name = "WASTE COIN";
+string public symbol = "WASTE";
+uint public decimals = 18;
+}
